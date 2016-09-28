d6480ea76fc58b1a332d484d0ef5efcbdb10e752
Chris Porras <cporras@ku.edu>
2016-09-28 10:28:30 -0500

    Multiplication Table finished

--- php/Multiplication_Table.php
+++ php/Multiplication_Table.php
@@ -5,22 +5,29 @@ function multiplication($x, $y){
 }
 
 for($i = 0; $i<101; $i++){
+  echo '<tr>';
   if($i == 0){
     for($k = 0; $k<101; $k++){
       if($k == 0){
-
+        echo '<td></td>';
       }
       else{
-        echo $k;
+        echo '<th>'.$k.'</th>';
       }
     }
   }
   else{
-    for ( $j = 1; $j<101; $j++){
-        echo multiplication($i,$j)."  ";
+    for ( $j = 0; $j<101; $j++){
+      if($j == 0){
+        echo '<th>'.$i.'</th>';
+      }
+      else{
+        echo '<td>'.multiplication($i,$j)."</td>";
+      }
     }
 
+
   }
-  echo "<br>";
+  echo '</tr>';
 }
 ?>
