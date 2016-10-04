a0c206fa1a70f4b93636111af597bfb62965c4db
Chris Porras <cporras@ku.edu>
2016-10-04 11:35:49 -0500

    Exercise 2 finished. Quiz html and php completed

--- php/Quiz.php
+++ php/Quiz.php
@@ -4,10 +4,34 @@
   $answer3 = $_POST["Question3"];
   $answer4 = $_POST["Question4"];
   $answer5 = $_POST["Question5"];
+  $score = 0;
   $submit = $_POST["Submit"];
-  echo $answer1."<br>";
-  echo $answer2."<br>";
-  echo $answer3."<br>";
-  echo $answer4."<br>";
-  echo $answer5."<br>";
+
+  if($answer1 == "Medivh"){
+    $score ++;
+  }
+  if($answer2 == "Llane Wrynn"){
+    $score ++;
+  }
+  if($answer3 == "Lordaeron"){
+    $score ++;
+  }
+  if($answer4 == "Orgrim Doomhammer"){
+    $score ++;
+  }
+  if($answer5 == "Capital City (Lordaeron)"){
+    $score ++;
+  }
+  echo "Your answer: ".$answer1."<br>";
+  echo "Correct answer: Medivh"."<br><br>";
+  echo "Your answer: ".$answer2."<br>";
+  echo "Correct answer: Llane Wrynn"."<br><br>";
+  echo "Your answer: ".$answer3."<br>";
+  echo "Correct answer: Lordaeron"."<br><br>";
+  echo "Your answer: ".$answer4."<br>";
+  echo "Correct answer: Orgrim Doomhammer"."<br><br>";
+  echo "Your answer: ".$answer5."<br>";
+  echo "Correct answer: Capital City (Lordaeron)"."<br><br>";
+  echo "Your score: ". $score."/5"."<br>";
+  echo "You got a ". $score * 20 ."%"."<br>";
 ?>
