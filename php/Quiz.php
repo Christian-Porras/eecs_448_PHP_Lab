aad3173f8ceb81419c02727d72ccccd572a40bcc
Chris Porras <cporras@ku.edu>
2016-10-04 09:06:24 -0500

    Questions created. PHP gets answers to questions and display when submit button is pressed

--- php/Quiz.php
+++ php/Quiz.php
@@ -1,5 +1,13 @@
 <?php
-  echo '<h1>'."Football (Soccer) Quiz Time".'</h1>';
-  echo "What year was the Premier League founded?"."<br>";
-  
+  $answer1 = $_POST["Question1"];
+  $answer2 = $_POST["Question2"];
+  $answer3 = $_POST["Question3"];
+  $answer4 = $_POST["Question4"];
+  $answer5 = $_POST["Question5"];
+  $submit = $_POST["Submit"];
+  echo $answer1."<br>";
+  echo $answer2."<br>";
+  echo $answer3."<br>";
+  echo $answer4."<br>";
+  echo $answer5."<br>";
 ?>
